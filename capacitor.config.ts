97dc867bfcb69787f399a64006cdd7fc5e834b28
jrivas <jrivas@bancomercantil.com>
2024-02-26 21:09:24 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ionic.01counterapp',
+  appName: '01counterapp',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
